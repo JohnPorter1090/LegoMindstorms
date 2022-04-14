05a63adae48be9311973d961f5aae8d2e797c1d6
JohnPorter1090 <101543357+JohnPorter1090@users.noreply.github.com>
2022-04-13 23:45:29 -0600

    Co-authored-by: svicchair64@gmail.com <svicchair64@gmail.com> Co-authored-by: Finnplayz <Finnplayz@users.noreply.github.com>

--- main.py
+++ main.py
@@ -18,74 +18,91 @@ ev3.speaker.set_speech_options(language='de', voice='m3', speed=10, pitch=50)
 ev3.speaker.set_volume(1000)
 gyro_sensor = GyroSensor(Port.S2)
 
-xy = y
+"""
+Gameplan:
+
+
+
+
+
+"""
+xy = "y"
 number = 0
 
 
 x_value = 0
-y _value = 0
+y_value = 0
 
-
+"""
+among = 1
+while True:
+    print("Patching Ticket #" + among)
+    among = among + among
+"""
 
 gyro_sensor.reset_angle(0)
 
 
 
 directional_degrees = 0
-def reset_gyro():
-    gyro_sensor.reset_angle(0)
-    while True:
-        if gyro_sensor.angle == 0:
-            break
-    while True:
-        if gyro_sensor.speed() == 0:
 
-def gyro_straight(distance):
-    target = gyro_sensor.angle()
-    gain = 3
-    robot.reset()
-
-    while robot.distance() < degrees:
-        correction = (target - gyro_sensor.angle())*gain
-        robot.drive(distance, correction)
-        if xy == x:
-            x_value += distance
-        elif xy == y:
-            y_value += distance
-
-
-def turn_left(degrees):
-    while gyro_sensor.angle > degrees:
-        motorb.run(degrees)
-    motorb.stop()
-    number += 1
-        directional_degrees += 90
-    if number % 2 == 1:
-        xy = x
-    elif number % 2 == 0:
-        xy = y
-    
-def get_correct_angel():
-    directrional_degrees 
+def get_correct_angle(directional_degrees):
     if directional_degrees == 360:
         directional_degrees = 0
     elif directional_degrees == -90:
         directional_degrees = 270
     elif directional_degrees == 450:
         directional_degrees = 90
-    elif direcional_degrees == -180:
+    elif directional_degrees == -180:
         directional_degrees = 180
 
-def turn_right(degrees):
-    while gyro_sensor.angle > degrees:
+
+def reset_gyro():
+    gyro_sensor.reset_angle(0)
+    while True:
+        if gyro_sensor.angle() == 0:
+            break
+    while True:
+        if gyro_sensor.speed() == 0:
+            break
+def gyro_straight(distance, x_coord, y_coord):
+    target = gyro_sensor.angle()
+    gain = 3
+    robot.reset()
+    while robot.distance() < distance:
+        correction = (target - gyro_sensor.angle())*gain
+        robot.drive(distance, correction)
+        if xy == "x":
+            x_coord += distance
+        elif xy == "y":
+            y_coord += distance
+
+
+def turn_left(degrees, number, directional_degrees):
+    while gyro_sensor.angle() > degrees:
+        motorb.run(degrees)
+    motorb.stop()
+    number += 1
+    directional_degrees += 90
+    get_correct_angle(directional_degrees)
+    if number % 2 == 1:
+        xy = "x"
+    elif number % 2 == 0:
+        xy = "y"
+    
+
+
+def turn_right(degrees, number, directional_degrees):
+    while gyro_sensor.angle() > degrees:
         motorc.run(degrees)
     motorc.stop()
     number += 1
-    directional_degrees -= 90
+    directional_degrees -= degrees
+    get_correct_angle(directional_degrees)
     if number % 2 == 1:
-        xy = x
+        xy = "x"
     elif number % 2 == 0:
-        xy = y
+        xy = "y"
 
 # Directional_Degrees
 # 0 facing  N   Y
@@ -93,10 +110,60 @@ def turn_right(degrees):
 # 180 facing  S   Y
 # 270 facing   W   X
 
-def go_to_coordinate(direction, x_coord, y_coord)
+def go_to_coordinate(direction, x_coord, y_coord, directional_degrees):
     while directional_degrees != direction:
         turn_right(90)
         directonal_degrees -= 90
+        get_correct_angle(directional_degrees)
+    if xy == "y":
+        robot.drive(y_coord, correction)
+        y_value = y_coord
+        turn_left(90, number)
+        robot.drive(x_coord,correction)
+        x_value = x_coord
+    elif xy == "x":
+        robot.drive(x_coord, correction)
+        x_value = x_coord
+        turn_left(90, number)
+        robot.drive(y_coord,correction)
+        y_value = y_coord
+    
+    
+    
+    """
+                                                                                    
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                           ,***,,,,,,,,,,,,,,,*//.                              
+                      /,,,,*******************,,,,,,,*          ......          
+                ..*,,,,******************************,,,,. . ...........        
+           .//////*//***********************************,,*...............      
+          (   .(/*/////*/////***,**,,....,,****************,*,,,,,,,,,...,      
+         .       ///((/((///,,//////..,,**********************,,,**,,,,,.,.     
+        .*/@     ./        (/////////*************************/*******,,..      
+       .,,@@         .       ////////////****************////*//*((((,,...      
+       ,(&@      . # %&.      ///////////////////////////////////((,,,...       
+       *..      ..%&&&@       ,////////////////////////////////((/*,,..,        
+       *,,.   .(...&&(        (//////////////////////////////((((/,,...         
+       ,,,,,....,..          ////////////////////////////((((((((*..,           
+       .,,,,.....,,...     .     ,/(/////////////////(/((((((((((,              
+        ,,*,,........... .               *(///((((((((((((((((((.               
+          ,,*,................                 /(((((((((((((((                 
+           .*,,*****/,.      ..                   . (((((((((*                  
+              *****,,,,................................((((,                    
+                 *********,,,,,,,,,............,,,,,,,,/.                       
+                     .*****************,,,,,,,******                            
+                             .,************..                                   
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+    """
 
 
 def go_to_zero_zero(target):
@@ -104,22 +171,30 @@ def go_to_zero_zero(target):
     gain = 3
     robot.reset() 
     correction = (target - gyro_sensor.angle())*gain
-    if xy = y:
+    if xy == "y":
         robot.drive(-y_value, correction)
         y_value = 0
         turn_left()
         robot.drive(-x_value,correction)
         x_value = 0
-    elif xy = x:
+    elif xy == "x":
         robot.drive(-x_value, correction)
         x_value = 0
         turn_left()
         robot.drive(-y_value,correction)
         y_value = 0
 
-turn_right(90)
+reset_gyro()
+print(directional_degrees)
+turn_right(90, number, directional_degrees)
+turn_left(180, number, directional_degrees)
+
+print(directional_degrees)
 
 
+"""
 while True:
-    gyro_straight(100)
-    
+    gyro_straight(100, x_value, y_value)
+"""
+
+
---
+++ newmain.py
@@ -0,0 +1,11 @@
+"""
+Gameplan:
+
+
+1. Robot is always at a set angle
+2. If detects offset, it gets back in line
+3. Start in bottom right corner/ top left corner
+4. Start by going forward
+5. 
+
+"""
