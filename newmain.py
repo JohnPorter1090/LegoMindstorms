b098363fb30f65a2146fbe60270bd9a0eab0770b
JohnPorter1090 <iainkennethcarter@gmail.com>
2022-04-14 16:55:00 -0600

    added "Absolute Straight"

--- newmain.py
+++ newmain.py
@@ -1,3 +1,19 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile, Font
+import time
+
+
+
+
+
+
+
+
 """
 Gameplan:
 
@@ -17,3 +33,34 @@ Gameplan:
 -Upon robot detected, do something (ram into maybe) and then leave using ultrasonic for position
 
 """
+
+ev3 = EV3Brick()
+motorc=Motor(Port.C) #right
+motorb=Motor(Port.B) #left
+chainsaw = Motor(Port.D)
+robot = DriveBase(motorb, motorc, wheel_diameter=55.5,
+axle_track=104)
+ev3.speaker.set_speech_options(language='de', voice='m3', speed=10, pitch=50)
+ev3.speaker.set_volume(1000)
+gyro_sensor = GyroSensor(Port.S2)
+
+
+gyro_sensor.reset_angle(0)
+
+def distance(millimeters): #converts distance in mm to 1/4 in
+    
+
+x =  0
+y = 0 
+
+def absolute_straight:
+    while True:
+        if gyro_sensor.angle < 1 and gyro_sensor.angle > -1:
+            robot.straight(1000)
+        elif gyro_sensor.angle < 180:
+            robot.turn(1)
+        elif gyro_sensor.angle > 180:
+            robot.turn(-1)
+        elif gyro_sensor.angle == 180:
+            robot.turn(180)
+        
