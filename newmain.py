385b5c772d177f228f8c0705c2c4de02e4ef932e
JohnPorter1090 <101543357+JohnPorter1090@users.noreply.github.com>
2022-04-14 00:41:15 -0600

    New Game Plan

--- newmain.py
+++ newmain.py
@@ -6,6 +6,15 @@ Gameplan:
 2. If detects offset, it gets back in line
 3. Start in bottom right corner/ top left corner
 4. Start by going forward
-5. 
+5. When color sensor detects other side of arena, continues to move forward for a few seconds, closes box and moves backwards to go home
+6. Ultrasonic on back detects back wall and starts the goal dropoff sequence once certain distance-FIND THIS DISTANCE WITH TEST to see when turn 
+    if whole robot + cap holder will stay in goal (check that against movement values of location to see if there is another robot)
+7. Drop off sequence: Check floor for correct color, then turn 90 drive to far end and open box, back up and continue sequence of picking up caps
+8. MAybe something cool at end like steal caps
+
+9. at end bulldoze other teams goal
+
+-Touch in front if robot detected goes to ultrasonic in back to get position
+-Upon robot detected, do something (ram into maybe) and then leave using ultrasonic for position
 
 """
